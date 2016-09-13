41e70dfa3f15126bad5743bc622b2f9dd5dc5c73
Jakub <mikrut.jakub@gmail.com>
2016-09-13 19:39:19 +0200

    generate error message

--- array.rb
+++ array.rb
@@ -2,7 +2,13 @@ require 'i18n'
 I18n.load_path = Dir['./locals/*.yml']
 
 class Array
-  def add(_attribute, message, _options)
-    self << I18n.t("errors.#{message}")
+  def add(attribute, message, _options)
+    self << error_message(attribute, message)
+  end
+
+  private
+
+  def error_message(attribute, message)
+    attribute.to_s + ' ' + I18n.t("errors.#{message}")
   end
 end
